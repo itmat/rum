0ee419a92f561de4d366de0b6fe82b077780f009
Mike DeLaurentis <delaurentis@gmail.com>
2012-01-24 16:07:05 -0500

    use strict in make_fasta_...

--- bin/make_fasta_files_for_master_list_of_genes.pl
+++ bin/make_fasta_files_for_master_list_of_genes.pl
@@ -17,38 +17,56 @@ for which there is no sequence in the <genome fasta> file.
 ";
 }
 
-$final_gene_info_file = $ARGV[3];
+use strict;
 
-our %exons;
+our %chr_hash1;
+our %chr_hash2;
+
+my (undef,
+    undef,
+    undef,
+    $final_gene_info_file) = @ARGV;
+
+sub read_chromosome_line {
+  use strict;
+  my ($in) = @_;
+  my $line = <$in>;
+  chomp($line);
+  $line =~ />(.*)/;
+  return $1; 
+}
 
 # Note: fasta file $ARGV[0] must have seq all on one line
-open(INFILE, $ARGV[0]) or die "ERROR: cannot open '$ARGV[0]' for reading\n";
-$line = <INFILE>;
+open my $infile, "<", $ARGV[0] or die "ERROR: cannot open '$ARGV[0]' for reading\n";
+
+my $line = <$infile>;
 chomp($line);
 $line =~ />(.*)/;
-$chr = $1; 
+my $chr = $1;
 print STDERR "$line\n";
 $chr_hash1{$chr}++;
 until($line eq '') {
-    $line = <INFILE>;
+    $line = <$infile>;
     chomp($line);
-    $seq = $line;
-    &get_exons($chr, $seq);
+    my $seq = $line;
+    open my $exon_in_file, $ARGV[1] or die "ERROR: cannot open '$ARGV[1]' for reading.\n";
+    my $exons = &get_exons($exon_in_file, $chr, $seq);
     print STDERR "done with exons for $chr\n";
-    &get_genes($chr, $seq);
+
+    open(my $gene_in_file, $ARGV[2]) or die "ERROR: cannot open '$ARGV[2]' for reading.\n";    
+    &get_genes($gene_in_file, $chr, $seq, $exons);
     print STDERR "done with genes for $chr\n";
-    $line = <INFILE>;
+    $line = <$infile>;
     chomp($line);
     print STDERR "$line\n";
     $line =~ />(.*)/;
     $chr = $1; 
     $chr_hash1{$chr}++;
 }
-close(INFILE);
 
-$str = "cat $ARGV[2]";
-$flag = 0;
-foreach $key (keys %chr_hash2) {
+my $str = "cat $ARGV[2]";
+my $flag = 0;
+foreach my $key (keys %chr_hash2) {
     if($chr_hash1{$key}+0==0) {
 	if($flag == 0) {
 	    print STDERR "no sequence for:\n$key\n";
@@ -65,35 +83,38 @@ if($flag == 1) {
     print STDERR "str:\n$str\n";
     `$str`;
 } else {
-    $fn = $ARGV[2];
-    `cp $fn $final_gene_info_file`;
+  my $fn = $ARGV[2];
+  `cp $fn $final_gene_info_file`;
 }
 
 sub get_exons () {
-    ($chr, $seq) = @_;
+  use strict;
+  my ($exon_in_file, $chr, $seq) = @_;
 
-    undef %exons;
-    open(EXONINFILE, $ARGV[1]) or die "ERROR: cannot open '$ARGV[1]' for reading.\n";
-    while($line2 = <EXONINFILE>) {
-	chomp($line2);
-	$line2 =~ /(.*):(\d+)-(\d+)/;
-	$CHR = $1;
-	$START = $2;
-	$END = $3;
-	$chr_hash2{$CHR}++;
-	if($CHR eq $chr) {
-	    $EXONSEQ = substr($seq,$START-1,$END-$START+1);
-	    $exons{$line2} = $EXONSEQ;
-	}
+  my %exons;
+
+  while (defined (my $line2 = <$exon_in_file>)) {
+    chomp($line2);
+    $line2 =~ /(.*):(\d+)-(\d+)/;
+    my $CHR = $1;
+    my $START = $2;
+    my $END = $3;
+    $chr_hash2{$CHR}++;
+    if($CHR eq $chr) {
+      my $EXONSEQ = substr($seq,$START-1,$END-$START+1);
+      $exons{$line2} = $EXONSEQ;
     }
-    close(EXONINFILE);
+  }
+
+  return \%exons;
 }
 
+=item get_genes
+
+=cut
 sub get_genes () {
   use strict;
-  my ($chr, $seq) = @_;
-  
-  open(my $gene_in_file, $ARGV[2]) or die "ERROR: cannot open '$ARGV[2]' for reading.\n";
+  my ($gene_in_file, $chr, $seq, $exons) = @_;
 
   while(defined (my $line2 = <$gene_in_file>)) {
     chomp($line2);
@@ -113,8 +134,8 @@ sub get_genes () {
         my $s = $STARTS[$i] + 1;  # add one because of the pesky zero based ucsc coords
         my $e = $ENDS[$i];  # don't add one to the end, because of the pesky half-open based ucsc coords
         my $ex = "$CHR:$s-$e";
-        $GENESEQ = $GENESEQ . $exons{$ex};
-        if(!($exons{$ex} =~ /\S/)) {
+        $GENESEQ = $GENESEQ . $exons->{$ex};
+        if(!($exons->{$ex} =~ /\S/)) {
           die "ERROR: exon for $ex not found.\n$line2\ni=$i\n";
         }
       }
