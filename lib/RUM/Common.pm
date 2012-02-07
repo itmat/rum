5986607a7eb861b661314b4303b71710635b5d08
Mike DeLaurentis <delaurentis@gmail.com>
2012-02-07 15:09:56 -0500

    Rename some local vars for clarity

--- lib/RUM/Common.pm
+++ lib/RUM/Common.pm
@@ -19,17 +19,17 @@ TODO: Document me
 sub getave {
     my ($spans_x) = @_;
 
-    my @SS3 = split(/, /, $spans_x);
-    my $spanave = 0;
-    my $spanlen = 0;
-    for(my $ss3=0; $ss3 < @SS3; $ss3++) {
-	my @SS4 = split(/-/, $SS3[$ss3]);
-	$spanave = $spanave + $SS4[1]*($SS4[1]+1)/2 - $SS4[0]*($SS4[0]-1)/2;
-	$spanlen = $spanlen + $SS4[1] - $SS4[0] + 1;
+    my @spans = split(/, /, $spans_x);
+    my $ave = 0;
+    my $len = 0;
+    for my $span (@spans) {
+	my ($start, $end) = split(/-/, $span);
+	$ave = $ave + $end*($end+1)/2 - $start*($start-1)/2;
+	$len = $len + $end - $start + 1;
     }
-    $spanave = $spanave / $spanlen;
+    $ave = $ave / $len;
 
-    return $spanave;
+    return $ave;
 }
 
 1;
