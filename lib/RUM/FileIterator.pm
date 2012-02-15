49954eb6840da2e0d25413cf75ec36903ff7d749
Mike DeLaurentis <delaurentis@gmail.com>
2012-02-15 14:17:58 -0500

    Refactor merge_sorted_RUM_files.pl
    + Move getNext out of it and put functionality in RUM::FileIterator,
      using the iterator pattern.
    + use strict

---
+++ lib/RUM/FileIterator.pm
@@ -0,0 +1,104 @@
+package RUM::FileIterator;
+
+use strict;
+use warnings;
+
+use Exporter qw(import);
+use Carp;
+
+our @EXPORT_OK = qw(file_iterator pop_it peek_it);
+
+
+sub file_iterator {
+    my ($filename) = @_;
+    open my $in, "<", $filename;
+ 
+    my $nextval = sub {
+        my $line1 = <$in>;
+        
+        # When we hit EOF, close the input and return undef for all
+        # subsequent values. We're using undef to indicate the end of
+        # the iterator.
+        unless (defined $line1) {
+            close $in;
+            return undef;
+        }
+
+        chomp($line1);
+
+        # This is basically the same as before, but rather than
+        # storing the values in several global hashes, we're creating
+        # a hash for each element in the iterator, keyed on chr,
+        # start, end, and entry.
+        my %res;
+        my @a = split(/\t/,$line1);
+        $res{chr} = $a[1];
+        $a[2] =~ /^(\d+)-/;
+        $res{start} = $1;
+        if($a[0] =~ /a/) {
+            $a[0] =~ /(\d+)/;
+            my $seqnum1 = $1;
+            my $line2 = <$in>;
+            chomp($line2);
+            my @b = split(/\t/,$line2);
+            $b[0] =~ /(\d+)/;
+            my $seqnum2 = $1;
+            if($seqnum1 == $seqnum2 && $b[0] =~ /b/) {
+                if($a[3] eq "+") {
+                    $b[2] =~ /-(\d+)$/;
+                    $res{end} = $1;
+                } else {
+                    $b[2] =~ /^(\d+)-/;
+                    $res{start} = $1;
+                    $a[2] =~ /-(\d+)$/;
+                    $res{end} = $1;
+                }
+                $res{entry} = $line1 . "\n" . $line2;
+            } else {
+                $a[2] =~ /-(\d+)$/;
+                $res{end} = $1;
+                # reset the file handle so the last line read will be read again
+                my $len = -1 * (1 + length($line2));
+                seek($in, $len, 1);
+                $res{entry} = $line1;
+            }
+        } else {
+            $a[2] =~ /-(\d+)$/;
+            $res{end} = $1;
+            $res{entry} = $line1;
+        }
+        chomp($res{entry});
+
+        return \%res;
+    };
+
+    # Call $nextval immediately so that a call to peek_it will work right
+    # away.
+    my $next = $nextval->();
+    
+    return sub {
+        my $cmd = shift() || "pop";
+        if ($cmd eq "peek") {
+            return $next;
+        }
+        elsif ($cmd eq "pop") {
+            my $last = $next;
+            $next = $nextval->();
+            return $last;
+        }
+    }
+}
+
+sub peek_it {
+    my $it = shift;
+    croak "I need an iterator" unless ref($it) =~ /CODE/;
+    return $it->("peek");
+}
+
+sub pop_it {
+    my $it = shift;
+    croak "I need an iterator" unless ref($it) =~ /CODE/;
+    return $it->("pop");
+}
+
+1;
--- orig/scripts/merge_sorted_RUM_files.pl
+++ orig/scripts/merge_sorted_RUM_files.pl
@@ -5,6 +5,7 @@ use lib "$Bin/../../lib";
 
 use RUM::Common qw(roman Roman isroman arabic);
 use RUM::ChrCmp qw(cmpChrs);
+use RUM::FileIterator qw(file_iterator pop_it peek_it);
 
 if(@ARGV<2) {
     die "Usage: merge_sorted_RUM_files.pl <outfile> <infile1> <infile2> [<infile3> ... <infileN>] [option]
@@ -20,353 +21,115 @@ if(@ARGV<2) {
 
 ";
 }
-$options = "false";
-$numfiles = 0;
-for($i=0; $i<@ARGV; $i++) {
+
+use strict;
+my $outfile = $ARGV[0];
+my $options_start_index;
+my $options = "false";
+my $numfiles = 0;
+for (my $i=0; $i<@ARGV; $i++) {
     if($ARGV[$i] =~ /^-/) {
-	$options = "true";
-	$options_start_index = $i;
-	$i = @ARGV;
+        $options = "true";
+        $options_start_index = $i;
+        $i = @ARGV;
     } else {
-	$numfiles = $i;
+        $numfiles = $i;
     }
 }
 
-$chunk_ids_file = "";
-if($options eq "true") {
-    for($i=$options_start_index; $i<@ARGV; $i++) {
-	if($ARGV[$i] eq '-chunk_ids_file') {
-	    $chunk_ids_file = $ARGV[$i+1];
-	    if(-e $chunk_ids_file) {
-		open(INFILE, $chunk_ids_file) or die "Error: cannot open '$chunk_ids_file' for reading.\n\n";
-		while($line = <INFILE>) {
-		    chomp($line);
-		    @a = split(/\t/,$line);
-		    $chunk_ids_mapping{$a[0]} = $a[1];
-		}
-		close(INFILE);
-	    } else {
+my $chunk_ids_file = "";
+my %chunk_ids_mapping;
 
-	    }
-	}
+if ($options eq "true") {
+    for (my $i = $options_start_index; $i<@ARGV; $i++) {
+        if ($ARGV[$i] eq '-chunk_ids_file') {
+            $chunk_ids_file = $ARGV[$i+1];
+            if (-e $chunk_ids_file) {
+                open(INFILE, $chunk_ids_file)
+                    or die "Error: cannot open '$chunk_ids_file' for reading.\n\n";
+                while (defined (my $line = <INFILE>)) {
+                    chomp($line);
+                    my @a = split(/\t/,$line);
+                    $chunk_ids_mapping{$a[0]} = $a[1];
+                }
+                close(INFILE);
+            } else {
+                    
+            }
+        }
     }
 }
-$outfile = $ARGV[0];
 
-if($numfiles == 1) {
-    $infile = $ARGV[1];
+if ($numfiles == 1) {
+    my $infile = $ARGV[1];
     `cp $infile $outfile`;
     exit(0);
 }
-for($i=0; $i<$numfiles; $i++) {
+
+my @file;
+for (my $i=0; $i<$numfiles; $i++) {
     $file[$i] = $ARGV[$i+1];
-    $j = $i+1;
-    if($chunk_ids_file =~ /\S/ && $chunk_ids_mapping{$j} =~ /\S/) {
-	$file[$i] =~ s/(\d|\.)+$//;
-	$file[$i] = $file[$i] . ".$j." . $chunk_ids_mapping{$j};
+    my $j = $i+1;
+    if ($chunk_ids_file =~ /\S/ && $chunk_ids_mapping{$j} =~ /\S/) {
+        $file[$i] =~ s/(\d|\.)+$//;
+        $file[$i] = $file[$i] . ".$j." . $chunk_ids_mapping{$j};
     }
 }
 open(OUTFILE, ">$outfile");
 
-for($i=0; $i<$numfiles; $i++) {
-    open $F1{$i}, $file[$i];
-}
+my @iters = map { file_iterator($_) } @file;
+my @iters = grep { peek_it($_) } @iters;
 
-for($i=0; $i<$numfiles; $i++) {
-    $mergeFLAG = 0;
-    getNext1($i);
-}
+print "My its are @iters", scalar(@iters), "\n";
+    
+while (@iters) {
+        
+    my $argmin = 0;
+    my $min = peek_it($iters[$argmin]);
+    for (my $i = 1; $i < @iters; $i++) {
 
-while($mergeFLAG < $numfiles) {
-    $S=0;
-    until($chr[$S] ne 'finished1234') {
-	$S++;
+        my $rec = peek_it($iters[$i]);
+
+        # Set $smaller to 1 if the next record from this iterator
+        # is smaller than the current minimum
+        my $smaller = 0;
+        if ($rec->{chr} eq $min->{chr}) {
+            if ($min->{start} > $rec->{start}) {
+                $smaller = 1;
+            } elsif ($min->{start} == $rec->{start}) {
+                if ($min->{end} > $rec->{end}) {
+                    $smaller = 1;
+                }
+            }
+        } else {
+            if (cmpChrs($rec->{chr}, $min->{chr}) < 0) {
+                $smaller = 1;
+            }
+        }
+
+        # If this one is smaller, set $argmin and $min
+        # appropriately
+        if ($smaller) {
+            $argmin = $i;
+            $min = peek_it($iters[$argmin]);
+        }
     }
-    $argmin = $S;
-    for($i=$S+1; $i<$numfiles; $i++) {
-	if(!($chr[$i] eq "finished1234")) {
-	    if($chr[$i] eq $chr[$argmin]) {
-  		if($start[$argmin] > $start[$i]) {
-		    $argmin = $i;
-		} elsif($start[$argmin] == $start[$i]) {
-  		    if($end[$argmin] > $end[$i]) {
- 			$argmin = $i;
-		    }
-		}
-	    } else  {
-		if(&cmpChrs2($chr[$i],$chr[$argmin]) == 1) {
-		    $argmin = $i;
-		}
-	    }
-	}
+        
+    if ($min->{entry} =~ /\S/) {
+        print OUTFILE "$min->{entry}\n";
+
     }
-    if($entry[$argmin] =~ /\S/) {
-	print OUTFILE "$entry[$argmin]\n";
+        
+    # Pop the iterator that we just printed a record from; this
+    # way the next iteration will be looking at the next value
+    pop_it($iters[$argmin]);
+
+    unless (peek_it($iters[$argmin])) {
+        print "I exhausted $argmin\n";
+        splice @iters, $argmin, 1;
     }
-    &getNext1($argmin);
 }
 close(OUTFILE);
-for($i=0; $i<$numfiles; $i++) {
-    close($F1{$i});
-}
-
-sub getNext1 () {
-    ($FN) = @_;
-    $FF = $F1{$FN};
-    $line1 = <$FF>;
-    chomp($line1);
-    if($line1 eq '') {
-	$mergeFLAG++;
-	$start[$FN] = 1000000000000;  # effectively infinity, no chromosome should be this large
-	$chr[$FN] = "finished1234";
-	$entry[$FN] = "";
-	return "";
-    }
-    @a = split(/\t/,$line1);
-    $chr[$FN] = $a[1];
-    $a[2] =~ /^(\d+)-/;
-    $start[$FN] = $1;
-    if($a[0] =~ /a/) {
-	$a[0] =~ /(\d+)/;
-	$seqnum1 = $1;
-	$line2 = <$FF>;
-	chomp($line2);
-	@b = split(/\t/,$line2);
-	$b[0] =~ /(\d+)/;
-	$seqnum2 = $1;
-	if($seqnum1 == $seqnum2 && $b[0] =~ /b/) {
-	    if($a[3] eq "+") {
-		$b[2] =~ /-(\d+)$/;
-		$end[$FN] = $1;
-	    } else {
-		$b[2] =~ /^(\d+)-/;
-		$start[$FN] = $1;
-		$a[2] =~ /-(\d+)$/;
-		$end[$FN] = $1;
-	    }
-	    $out1 = $line1 . "\n" . $line2;
-	} else {
-	    $a[2] =~ /-(\d+)$/;
-	    $end[$FN] = $1;
-	    # reset the file handle so the last line read will be read again
-	    $len = -1 * (1 + length($line2));
-	    seek($FF, $len, 1);
-	    $out1 = $line1;
-	}
-    } else {
-	$a[2] =~ /-(\d+)$/;
-	$end[$FN] = $1;
-	$out1 = $line1;
-    }
-    chomp($out1);
-
-    $entry[$FN] = $out1;
-}
-
-sub cmpChrs2 () {
-    ($a2_c, $b2_c) = @_;
-    $a2_c = lc($a2_c);
-    $b2_c = lc($b2_c);
-    if($a2_c eq 'finished1234') {
-	return -1;
-    }
-    if($b2_c eq 'finished1234') {
-	return 1;
-    }
-    if($a2_c =~ /^\d+$/ && !($b2_c =~ /^\d+$/)) {
-        return 1;
-    }
-    if($b2_c =~ /^\d+$/ && !($a2_c =~ /^\d+$/)) {
-        return -1;
-    }
-    if($a2_c =~ /^[ivxym]+$/ && !($b2_c =~ /^[ivxym]+$/)) {
-        return 1;
-    }
-    if($b2_c =~ /^[ivxym]+$/ && !($a2_c =~ /^[ivxym]+$/)) {
-        return -1;
-    }
-    if($a2_c eq 'm' && ($b2_c eq 'y' || $b2_c eq 'x')) {
-        return -1;
-    }
-    if($b2_c eq 'm' && ($a2_c eq 'y' || $a2_c eq 'x')) {
-        return 1;
-    }
-    if($a2_c =~ /^[ivx]+$/ && $b2_c =~ /^[ivx]+$/) {
-        $a2_c = "chr" . $a2_c;
-        $b2_c = "chr" . $b2_c;
-    }
-    if($a2_c =~ /$b2_c/) {
-	return -1;
-    }
-    if($b2_c =~ /$a2_c/) {
-	return 1;
-    }
-    # dealing with roman numerals starts here
-    if($a2_c =~ /chr([ivx]+)/ && $b2_c =~ /chr([ivx]+)/) {
-	$a2_c =~ /chr([ivx]+)/;
-	$a2_roman = $1;
-	$b2_c =~ /chr([ivx]+)/;
-	my $b2_roman = $1;
-	my $a2_arabic = arabic($a2_roman);
-        my $b2_arabic = arabic($b2_roman);
-	if($a2_arabic > $b2_arabic) {
-	    return -1;
-	} 
-	if($a2_arabic < $b2_arabic) {
-	    return 1;
-	}
-	if($a2_arabic == $b2_arabic) {
-	    my $tempa = $a2_c;
-	    my $tempb = $b2_c;
-	    $tempa =~ s/chr([ivx]+)//;
-	    $tempb =~ s/chr([ivx]+)//;
-	    my %temphash;
-	    $temphash{$tempa}=1;
-	    $temphash{$tempb}=1;
-	    foreach my $tempkey (sort {cmpChrs($a,$b)} keys %temphash) {
-		if($tempkey eq $tempa) {
-		    return 1;
-		} else {
-		    return -1;
-		}
-	    }
-	}
-    }
-    if($b2_c =~ /chr([ivx]+)/ && !($a2_c =~ /chr([a-z]+)/) && !($a2_c =~ /chr(\d+)/)) {
-	return -1;
-    }
-    if($a2_c =~ /chr([ivx]+)/ && !($b2_c =~ /chr([a-z]+)/) && !($b2_c =~ /chr(\d+)/)) {
-	return 1;
-    }
-    if($b2_c =~ /m$/ && $a2_c =~ /vi+/) {
-	return 1;
-    }
-    if($a2_c =~ /m$/ && $b2_c =~ /vi+/) {
-	return -1;
-    }
-
-    # roman numerals ends here
-    if($a2_c =~ /chr(\d+)$/ && $b2_c =~ /chr.*_/) {
-        return 1;
-    }
-    if($b2_c =~ /chr(\d+)$/ && $a2_c =~ /chr.*_/) {
-        return -1;
-    }
-    if($a2_c =~ /chr([a-z])$/ && $b2_c =~ /chr.*_/) {
-        return 1;
-    }
-    if($b2_c =~ /chr([a-z])$/ && $a2_c =~ /chr.*_/) {
-        return -1;
-    }
-    if($a2_c =~ /chr(\d+)/) {
-        $numa = $1;
-        if($b2_c =~ /chr(\d+)/) {
-            $numb = $1;
-            if($numa < $numb) {
-                return 1;
-            }
-	    if($numa > $numb) {
-                return -1;
-            }
-	    if($numa == $numb) {
-		$tempa = $a2_c;
-		$tempb = $b2_c;
-		$tempa =~ s/chr\d+//;
-		$tempb =~ s/chr\d+//;
-		my %temphash;
-		$temphash{$tempa}=1;
-		$temphash{$tempb}=1;
-		foreach my $tempkey (sort {cmpChrs($a,$b)} keys %temphash) {
-		    if($tempkey eq $tempa) {
-			return 1;
-		    } else {
-			return -1;
-		    }
-		}
-	    }
-        } else {
-            return 1;
-        }
-    }
-    if($a2_c =~ /chrx(.*)/ && ($b2_c =~ /chr(y|m)$1/)) {
-	return 1;
-    }
-    if($b2_c =~ /chrx(.*)/ && ($a2_c =~ /chr(y|m)$1/)) {
-	return -1;
-    }
-    if($a2_c =~ /chry(.*)/ && ($b2_c =~ /chrm$1/)) {
-	return 1;
-    }
-    if($b2_c =~ /chry(.*)/ && ($a2_c =~ /chrm$1/)) {
-	return -1;
-    }
-    if($a2_c =~ /chr\d/ && !($b2_c =~ /chr[^\d]/)) {
-	return 1;
-    }
-    if($b2_c =~ /chr\d/ && !($a2_c =~ /chr[^\d]/)) {
-	return -1;
-    }
-    if($a2_c =~ /chr[^xy\d]/ && (($b2_c =~ /chrx/) || ($b2_c =~ /chry/))) {
-        return -1;
-    }
-    if($b2_c =~ /chr[^xy\d]/ && (($a2_c =~ /chrx/) || ($a2_c =~ /chry/))) {
-        return 1;
-    }
-    if($a2_c =~ /chr(\d+)/ && !($b2_c =~ /chr(\d+)/)) {
-        return 1;
-    }
-    if($b2_c =~ /chr(\d+)/ && !($a2_c =~ /chr(\d+)/)) {
-        return -1;
-    }
-    if($a2_c =~ /chr([a-z])/ && !($b2_c =~ /chr(\d+)/) && !($b2_c =~ /chr[a-z]+/)) {
-        return 1;
-    }
-    if($b2_c =~ /chr([a-z])/ && !($a2_c =~ /chr(\d+)/) && !($a2_c =~ /chr[a-z]+/)) {
-        return -1;
-    }
-    if($a2_c =~ /chr([a-z]+)/) {
-        my $letter_a = $1;
-        if($b2_c =~ /chr([a-z]+)/) {
-            my $letter_b = $1;
-            if($letter_a lt $letter_b) {return 1;}
-	    if($letter_a gt $letter_b) {return -1;}
-        } else {
-            return -1;
-        }
-    }
-    $flag_c = 0;
-    while($flag_c == 0) {
-        $flag_c = 1;
-        if($a2_c =~ /^([^\d]*)(\d+)/) {
-            my $stem1_c = $1;
-            my $num1_c = $2;
-            if($b2_c =~ /^([^\d]*)(\d+)/) {
-                $stem2_c = $1;
-                $num2_c = $2;
-                if($stem1_c eq $stem2_c && $num1_c < $num2_c) {
-                    return 1;
-                }
-                if($stem1_c eq $stem2_c && $num1_c > $num2_c) {
-                    return -1;
-                }
-                if($stem1_c eq $stem2_c && $num1_c == $num2_c) {
-                    $a2_c =~ s/^$stem1_c$num1_c//;
-                    $b2_c =~ s/^$stem2_c$num2_c//;
-                    $flag_c = 0;
-                }
-            }
-        }
-    }
-    if($a2_c le $b2_c) {
-	return 1;
-    }
-    if($b2_c le $a2_c) {
-	return -1;
-    }
 
 
-    return 1;
-}
-
-
-
+    
