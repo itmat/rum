e58e17e318ab8f35c1be8d41547323eb8d235cdf
Mike DeLaurentis <delaurentis@gmail.com>
2012-02-20 14:40:47 -0500

    Make wait.pl use strict

--- orig/scripts/wait.pl
+++ orig/scripts/wait.pl
@@ -1,14 +1,10 @@
-$dir = $ARGV[0];
-$JID = $ARGV[1];
+use strict;
+use warnings;
+
+my ($dir, $jid) = @ARGV;
+
 $dir =~ s!/$!!;
 
-$doneflag = 0;
-while($doneflag == 0) {
-    $doneflag = 1;
-    if(!(-e "$dir/$JID")) {
-	$doneflag = 0;
-    }
-    if($doneflag == 0) {
-	sleep(10);
-    }
+until (-e "$dir/$jid") {
+    sleep(10);
 }
