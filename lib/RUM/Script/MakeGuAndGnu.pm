d41bc71b31eb7efe3da22d0bfc22ae65cf8194e8
Mike DeLaurentis <delaurentis@gmail.com>
2012-03-07 16:27:35 -0500

    Format usage doc as pod for make_GU_and_GNU

--- lib/RUM/Script/MakeGuAndGnu.pm
+++ lib/RUM/Script/MakeGuAndGnu.pm
@@ -1,76 +1,13 @@
 package RUM::Script::MakeGuAndGnu;
 
+use Pod::Usage;
+
 sub main {
 
 $|=1;
 
 if(@ARGV < 4) {
-    print STDERR 
-"
-Usage: make_GU_and_GNU.pl <input_filename> <gu_filename> <tu_filename> <type> [options]
-
-  Where: <input_filename> is the file output from sort_bowtie.pl
-
-         <gu_filename> is the name of the file to be written that will contain
-                       unique genome alignments
-
-         <gnu_filename> is the name of the file to be written that will contain
-                        non-nique genome alignments
-
-         <type> is 'single' for single-end reads, or 'paired' for paired-end reads
-
-  Options:
-         -maxpairdist N : N is an integer greater than zero representing
-                          the furthest apart the forward and reverse reads
-                          can be.  They could be separated by an exon/exon
-                          junction so this number can be as large as the largest
-                          intron.  Default value = 500,000
-
-  INPUT:
-  -----
-  This script takes the output of a bowtie mapping against the genome, which has
-  been sorted by sort_bowtie.pl, and parses it to have the four columns:
-        1) read name
-        2) chromosome
-        3) span
-        4) sequence
-  A line of the (input) bowtie file should look like:
-  seq.1a   -   chr14   1031657   CACCTAATCATACAAGTTTGGCTAGTGGAAAA
-
-  Sequence names are expected to be of the form seq.Na where N in an integer
-  greater than 0.  The 'a' signifies this is a 'forward' read, and 'b' signifies
-  'reverse' reads.  The file may consist of all forward reads (single-end data), or
-  it may have both forward and reverse reads (paired-end data).  Even if single-end
-  the sequence names still must end with an 'a'.
-
-  OUTPUT:
-  ------
-  The line above is modified by the script to be:
-  seq.1a   chr14   1031658-1031689   CACCTAATCATACAAGTTTGGCTAGTGGAAAA
-
-  In the case of single-end reads, if there is a unique such line for seq.1a then
-  it is written to the file specified by <gu_filename>.  If there are multiple lines for
-  seq.1a then they are all written to the file specified by <gnu_filename>.
-
-  In the case of paired-end reads the script tries to match up entries for seq.1a
-  and seq.1b consistently, which means:
-        1) both reads are on the same chromosome
-        2) the two reads map in opposite orientations
-        3) the start of reads are further apart than ends of reads
-           and no further apart than $max_distance_between_paired_reads
-
-  If the two reads do not overlap then the consistent mapper is represented by two
-  consecutive lines, the forward (a) read first and the reverse (b) read second.
-  If the two reads overlap then they two lines are merged into one line and the
-  a/b designation is removed.
-
-  If there is a unique consistent mapper it is written to the file specified  by
-  <gu_filename>.  If there are multiple consistent mappers they are all written to
-  the file specified by <gnu_filename>.  If only the forward or reverse read map
-  then it does not write anything.
-
-";
-    exit(1);
+    pod2usage();
 }
 
 $infile = $ARGV[0];
