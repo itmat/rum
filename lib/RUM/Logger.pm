38d444a068f3f80118e8aa597a0d8d2c8648dfbe
Mike DeLaurentis <delaurentis@gmail.com>
2012-03-07 14:04:09 -0500

    Pod

--- lib/RUM/Logger.pm
+++ lib/RUM/Logger.pm
@@ -1,9 +1,23 @@
-###############################################################################
-##
-## Logging
-## 
 
 package RUM::Logger;
+
+=head1 NAME
+
+RUM::Logger - Simple logger for when Log4perl isn't installed
+
+=head1 SYNOPSIS
+
+Please see RUM::Logging.
+
+=head1 DESCRIPTION
+
+RUM::Logging->get_logger() will test to see if Log4perl is installed,
+and if so, use its loggers. If not, it will return instances of this
+class. This class just mimics some of the simplest features of a
+Log4perl logger. If a non-existent method is called on an instance of
+this class, we simply warn rather than die. 
+
+=cut
 
 use strict;
 use warnings;
@@ -21,22 +35,56 @@ our @LEVEL_NAMES = qw(TRACE DEBUG INFO WARN ERROR FATAL);
 
 our $FH;
 
+=head1 CLASS METHODS
+
+=over 4
+
+=item RUM::Logger->init()
+
+Do not call this directly; let RUM::Logging do it. Initialize the
+logger by opening the output filehandle.
+
+=cut
+
 sub init {
     open $FH, ">>", "rum.log" unless $FH;
 }
 
+=item RUM::Logger->get_logger()
+
+Do not call this directly; let RUM::Logging do it. Return a logger
+with the given name, creating one if one doesn't already exist.
+
+=cut
+
 sub get_logger {
     my ($class, $name) = @_;
-    $LOGGERS{$name} ||= $class->new($name);
+
+    $LOGGERS{$name} ||= $class->_new($name);
 }
 
-sub new {
+sub _new {
     my ($class, $name) = @_;
     my $self = {};
     $self->{threshold} = $DEFAULT_THRESHOLD;
     $self->{name} = $name;
     return bless $self, $class;
 }
+
+=back
+
+=head1 INSTANCE METHODS
+
+=over 4
+
+=item log($level, $msg)
+
+Print the given message if the given level is above my threshold. If
+the message came from a RUM::Script::* logger, print it to the screen
+as well as the log file, since it was probably intended for user
+consumption.
+
+=cut
 
 sub log {
     my ($self, $level, $msg) = @_;
@@ -51,12 +99,77 @@ sub log {
     }
 }
 
+=back
+
+=head2 Logging messages
+
+These methods send logging messages at different levels:
+
+=over 4
+
+=item trace($msg)
+
+=item debug($msg)
+
+=item info($msg)
+
+=item warn($msg)
+
+=item error($msg)
+
+=item fatal($msg)
+
+=back
+
+=cut
+
 sub trace { shift->log($TRACE, @_) }
 sub debug { shift->log($DEBUG, @_) }
 sub info  { shift->log($INFO,  @_) }
 sub warn  { shift->log($WARN,  @_) }
 sub error { shift->log($ERROR, @_) }
 sub fatal { shift->log($FATAL, @_) }
+
+=head2 Logging and exiting
+
+=over 4
+
+=item logdie($msg)
+
+Log a message and die with it.
+
+=cut
+
+sub logdie { 
+    my ($self, $msg) = @_;
+    $self->fatal($msg);
+    die $msg;
+}
+
+=back
+
+=head2 Testing the log level
+
+These methods return true if this logger will print messages at or
+above a certain level:
+
+=over 4
+
+=item is_trace
+
+=item is_debug
+
+=item is_info
+
+=item is_warn
+
+=item is_error
+
+=item is_fatal
+
+=back
+
+=cut
 
 sub is_trace { shift->{threshold} >= $TRACE }
 sub is_debug { shift->{threshold} >= $DEBUG }
@@ -65,11 +178,21 @@ sub is_warn  { shift->{threshold} >= $WARN  }
 sub is_error { shift->{threshold} >= $ERROR }
 sub is_fatal { shift->{threshold} >= $FATAL }
 
-sub logdie { 
-    my ($self, $msg) = @_;
-    $self->fatal($msg);
-    die $msg;
-}
+=head2 Adjusting the threshold
+
+=over 4
+
+=item more_logging($delta)
+
+Increment the threshold by $delta.
+
+=item less_logging($delta)
+
+Decrement the threshold by $delta.
+
+=back
+
+=cut
 
 sub less_logging { $_[0]->{threshold} += $_[1] };
 sub more_logging { $_[0]->{threshold} -= $_[1] };
@@ -78,5 +201,15 @@ our $AUTOLOAD;
 sub AUTOLOAD {
     carp "Method $AUTOLOAD does not exist in " . __PACKAGE__ unless $AUTOLOAD eq "RUM::Logger::DESTROY";
 }
+
+=head1 AUTHOR
+
+Mike DeLaurentis (delaurentis@gmail.com)
+
+=head1 COPYRIGHT
+
+Copyright 2012 University of Pennsylvania
+
+=cut
 
 1;
