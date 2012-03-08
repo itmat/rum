e181d2f9af43bfc48f694b6634c86849f00ea703
Mike DeLaurentis <delaurentis@gmail.com>
2012-03-08 17:03:09 -0500

    Move make_unmapped_File into package

---
+++ lib/RUM/Script/MakeUnmappedFile.pm
@@ -0,0 +1,89 @@
+package RUM::Script::MakeUnmappedFile;
+
+use RUM::Logging;
+use RUM::Usage;
+use Getopt::Long;
+
+sub main {
+
+    # FIX THIS SO THAT READS CAN SPAN MORE THAN ONE LINE IN THE FASTA FILE
+    $infile = $ARGV[0];
+    $infile1 = $ARGV[1];
+    $infile2 = $ARGV[2];
+    $outfile = $ARGV[3];
+    $type = $ARGV[4];
+    $typerecognized = 1;
+    if ($type eq "single") {
+        $paired_end = "false";
+        $typerecognized = 0;
+    }
+    if ($type eq "paired") {
+        $paired_end = "true";
+        $typerecognized = 0;
+    }
+    if ($typerecognized == 1) {
+        die "\nERROR: in script make_unmapped.pl: type '$type' not recognized.  Must be 'single' or 'paired'.\n";
+    }
+
+    open(INFILE, $infile1) or die "\nERROR: in script make_unmapped.pl: Cannot open file '$infile1' for reading\n";
+    while ($line = <INFILE>) {
+        chomp($line);
+        $line =~ s/\t.*//;
+        if (!($line =~ /(a|b)/)) {
+            $bu{$line}=2;
+        } else {
+            $line =~ s/(a|b)//;
+            $bu{$line}++;
+        }
+    }
+    close(INFILE);
+
+    open(INFILE, $infile2) or die "\nERROR: in script make_unmapped.pl: Cannot open file '$infile2' for reading\n";
+    while ($line = <INFILE>) {
+        chomp($line);
+        $line =~ s/\t.*//;
+        $line =~ s/(a|b)//;
+        $bnu{$line}++;
+    }
+    close(INFILE);
+
+    open(INFILE, $infile) or die "\nERROR: in script make_unmapped.pl: Cannot open file '$infile' for reading\n";
+
+    open(OUTFILE, ">$outfile") or die "\nERROR: in script make_unmapped.pl: Cannot open file '$outfile' for writing\n";
+
+    while ($line = <INFILE>) {
+        chomp($line);
+        if ($line =~ /^>(seq.\d+)/) {
+            $seq = $1;
+            if ($paired_end eq "true") {
+                if ($bu{$seq}+0 < 2 && !($bnu{$seq} =~ /\S/)) {
+                    $line_hold = $line;
+                    $line = <INFILE>;
+                    chomp($line);
+                    print OUTFILE "$line_hold\n";
+                    print OUTFILE "$line\n";
+                    $line_hold = <INFILE>;
+                    chomp($line_hold);
+                    $line = <INFILE>;
+                    chomp($line);
+                    print OUTFILE "$line_hold\n";
+                    print OUTFILE "$line\n";
+                }
+            } else {
+                if ($bu{$seq}+0 < 1 && !($bnu{$seq} =~ /\S/)) {
+                    $line_hold = $line;
+                    $line = <INFILE>;
+                    chomp($line);
+                    print OUTFILE "$line_hold\n";
+                    print OUTFILE "$line\n";
+                }
+            }
+        }
+    }
+    close(INFILE);
+    close(OUTFILE);
+
+    print "Starting BLAT on '$outfile'.\n";
+}
+
+1;
---
+++ t/19-make-unmapped-file.t
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 1;
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
+use RUM::Script::MakeUnmappedFile;
+use RUM::TestUtils qw(no_diffs);
+use File::Temp;
+
+my $tempdir = "$Bin/tmp";
+my $expected_dir = "$Bin/expected/make_unmapped_file";
+my $in_dir       = "$Bin/data/make_unmapped_file";
+
+sub temp_filename {
+    my ($template) = @_;
+    File::Temp->new(
+        DIR => "$Bin/tmp",
+        UNLINK => 0,
+        TEMPLATE => $template);
+}
+
+my $reads      = "$in_dir/reads.fa.1";
+my $unique     = "$in_dir/BowtieUnique.1";
+my $non_unique = "$in_dir/BowtieNU.1";
+
+sub merge_ok {
+    my $unmapped  = temp_filename("unmapped.XXXXXX");
+    @ARGV = ($reads, $unique, $non_unique, $unmapped, "paired");
+    
+    RUM::Script::MakeUnmappedFile->main();
+    no_diffs($unmapped, "$expected_dir/R.1");
+}
+
+merge_ok();
+
