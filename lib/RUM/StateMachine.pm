2a34715beb8e13e282a10c7fb065569ec34ea6a9
Mike DeLaurentis <delaurentis@gmail.com>
2012-03-20 13:44:16 -0400

    Clean up state machine a bit

--- lib/RUM/StateMachine.pm
+++ lib/RUM/StateMachine.pm
@@ -15,69 +15,53 @@ sub is_array_ref_of_strings {
 sub new {
     my ($class, %options) = @_;
 
-    for my $key (qw(state_flags instructions start_flags goal_flags)) {
-        is_array_ref_of_strings($options{$key}) 
-            or croak "$key must be an ARRAY ref of strings";
-    }
-
-    my @flags = @{ $options{state_flags} };
-
-    my @instructions = @{ $options{instructions} };
-
-    # Build a hash mapping each flag to its mask
-    my %flags;
-    my $i;
-    for my $flag (@{ $options{state_flags}}) {
-        $flags{$flag} = 1 << $i++;
-    }
-
-    # Function that takes an array ref of symbols and returns the mask
-    # for the set of states that have those flags set.
-    my $flags_to_state = sub {
-        my ($symbols) = @_;
-        my @flags = map { $flags{$_} or croak "Undefined state flag $_" } @$symbols;
-        return 0 unless @flags;
-        return reduce { $a | $b } @flags;
-    };
-
-    my %transitions = map { ($_ => {}) } @instructions;
-    
-    for my $t (@{ $options{transitions} }) {
-        ref($t) =~ /^ARRAY/ or croak "Transition must be an array ref";
-
-        my ($pre, $post, $instruction) = @$t;
-
-        is_array_ref_of_strings($pre) or croak "Precondition must be an array ref, not $post";
-        is_array_ref_of_strings($post) or croak "Postcondition must be an array ref, not $post";
-
-        $pre = $flags_to_state->($pre);
-        $post = $flags_to_state->($post);
-
-        print "Adding $instruction, $pre, $post\n";
-
-        $transitions{$instruction} or croak "Undefined instruction $instruction";
-
-        $transitions{$instruction}{$pre} = $post;
-    }
+    my $start = delete($options{start}) || 0;
 
     my $self = {};
-    $self->{flags}          = \@flags;
-    $self->{start_state}    = $flags_to_state->($options{start_flags});
-    $self->{goal_mask}      = $flags_to_state->($options{goal_flags});
-    $self->{transitions}    = \%transitions;
-    $self->{flags_to_state} = $flags_to_state;
-
+    $self->{start}        = $start;
+    $self->{flags}        = {};
+    $self->{transitions}  = {};
+    $self->{goal_mask}    = 0;
     return bless $self, $class;
 }
 
-sub requirements {
-    my ($self, $instruction) = @_;
-    return keys %{ $self->{transitions}{$instruction} };
+sub set_goal {
+    my ($self, $mask) = @_;
+    $self->{goal_mask} = $mask;
 }
 
-sub production {
-    my ($self, $instruction, $pre) = @_;
-    return $self->{transitions}{$instruction}{$pre};
+sub flag {
+    my ($self, $flag) = @_;
+    my $n = $self->flags;
+    return $self->{flags}{$flag} ||= 1 << $n;
+}
+
+sub start {
+    shift->{start};
+}
+
+sub flags {
+    my $self = shift;
+    my %flags = %{ $self->{flags} };
+    return keys %flags unless @_;
+    my $state = shift;
+    return grep { $state & $flags{$_} } keys %flags;
+}
+
+sub state {
+    my ($self, @flags) = @_;
+    my $state = 0;
+    for my $flag (@flags) {
+        my $bit = $self->{flags}{$flag}
+            or croak "Undefined flag $flag";
+        $state |= $bit;
+    }
+    return $state;
+}
+
+sub add {
+    my ($self, $pre, $post, $instruction) = @_;
+    $self->{transitions}{$instruction}{$pre} = $post;
 }
 
 sub transition {
@@ -104,6 +88,20 @@ sub transition {
     return $state;
 }
 
+sub transitions {
+    return %{ shift->{transitions} };
+}
+
+sub requirements {
+    my ($self, $instruction) = @_;
+    return keys %{ $self->{transitions}{$instruction} };
+}
+
+sub production {
+    my ($self, $instruction, $pre) = @_;
+    return $self->{transitions}{$instruction}{$pre};
+}
+
 sub adjacent {
     my ($self, $state) = @_;
 
@@ -112,10 +110,13 @@ sub adjacent {
     for ($self->instructions) {
         my $new_state = $self->transition($state, $_);
         $transitions{$_} = $new_state unless $new_state == $state;
-        print "Adding $_ => $new_state\n";
     }
 
     return %transitions;
+}
+
+sub instructions {
+    return keys %{ shift->{transitions} };
 }
 
 sub is_goal {
@@ -128,7 +129,7 @@ sub generate {
     my ($self) = @_;
 
     my @plan;
-    my $state = $self->start_state;
+    my $state = $self->start;
 
     while (!$self->is_goal($state)) {
 
@@ -144,31 +145,10 @@ sub generate {
     }
     return \@plan;
 }
+1;
 
-sub flags_to_state {
+__END__
 
-    my $self = shift;
-
-    my $flags = @_ == 1 && ref($_[0]) =~ /^ARRAY/ ? shift : [@_];
-    
-    return $self->{flags_to_state}->($flags);
-}
-
-sub flags {
-    return @{ shift->{flags} };
-}
-
-sub instructions {
-    return keys %{ shift->{transitions} };
-}
-
-sub transitions {
-    return %{ shift->{transitions} };
-}
-
-sub start_state {
-    return shift->{start_state};
-}
 
 sub state_to_flags {
     my ($self, $state) = @_;
@@ -181,8 +161,6 @@ sub state_to_flags {
     return \@flags;
 }
 
-1;
-__END__
 
 sub to_makefile {
     my ($self, @args) = @_;
--- t/04-state-machine.t
+++ t/04-state-machine.t
@@ -1,69 +1,57 @@
 #!perl
 # -*- cperl -*-
 
-use Test::More tests => 4;
+use Test::More tests => 20;
 use Test::Exception;
 use lib "lib";
 
 use strict;
 use warnings;
-use Log::Log4perl qw(:easy);
-use FindBin qw($Bin);
-use File::Temp qw(tempdir);
 
 BEGIN { 
     use_ok('RUM::StateMachine');
 }                                               
 
-my $m = RUM::StateMachine->new(
-    state_flags => [qw(letters numbers combined sorted)],
-    instructions => [qw(cut_first_col
-                        cut_second_col
-                        cat_cols
-                        sort)],
-    start_flags => [],
-    goal_flags  => [qw(sorted)],
-    transitions => [
-        [[],                     ["letters"],  "cut_first_col"],
-        [[],                     ["numbers"],  "cut_second_col"],
-        [["letters", "numbers"], ["combined"], "cat_cols"],
-        [["combined"],           ["sorted"],   "sort"]]
-);
+my $m = RUM::StateMachine->new(start => 0);
 
-is_deeply([$m->flags], [qw(letters numbers combined sorted)]);
+my $start = $m->start;
+my $letters = $m->flag("letters");
+my $numbers = $m->flag("numbers");
+my $combined = $m->flag("combined");
+my $sorted = $m->flag("sorted");
 
-is($m->{start_state}, 0, "Start state");
-is($m->flags_to_state([]), 0, "Flags to state with no flags");
-is($m->flags_to_state(["letters"]), 1, "Flags to state with letters");
-is($m->flags_to_state("letters"), 1, "Flags to state with letters");
-is($m->flags_to_state(["numbers"]), 2, "Flags to state with numbers");
-is($m->flags_to_state(["letters", "numbers"]), 3, 
-   "Flags to state with letters and numbers");
-is($m->flags_to_state(["letters", "numbers", "combined"]), 7, 
-   "Flags to state with letters, numbers, and combined");
-is($m->flags_to_state(["letters", "numbers", "combined", "sorted"]), 15,
-   "Flags to state with all states");
+$m->add($start, $letters, "cut_first_col");
+$m->add($start, $numbers, "cut_second_col");
+$m->add($letters | $numbers, $combined, "cat_cols");
+$m->add($combined, $sorted, "sort");
 
-is_deeply($m->state_to_flags(0),
-          [],
-          "state_to_flags(0)");
-is_deeply($m->state_to_flags(7),
-          ["letters", "numbers", "combined"],
-          "state_to_flags(7)");
-    
-my $start   = $m->flags_to_state();
-my $letters = $m->flags_to_state("letters");
-my $numbers = $m->flags_to_state("numbers");
-my $combined = $m->flags_to_state("combined");
-my $sorted   = $m->flags_to_state("sorted");
+$m->set_goal($sorted);
+
+is_deeply([sort $m->flags], [qw(combined letters numbers sorted)],
+      "flags");
+
+is($start, 0, "start");
+is($letters, 1, "letters");
+is($numbers, 2, "numbers");
+is($combined, 4, "combined");
+is($sorted,   8, "sorted");
+is($m->flag("letters"), 1, "flag again");
+
+is_deeply([sort $m->flags($combined | $letters)],
+          ["combined", "letters"],
+          "Flags with arg");
+
+is($m->state("letters"), $letters, "state");
+is($m->state("letters", "numbers"), $letters | $numbers, "state");
+is($m->state("letters", "numbers", "combined"), $letters | $numbers | $combined,
+   "state");
 
 is($m->transition($start, "cut_first_col"), $letters,
    "start, cut_first_col -> letters");
-is($m->transition($start, "cut_first_col"), $letters,
-   "start, cut_first_col -> letters");
+is($m->transition($start, "cut_second_col"), $numbers,
+   "start, cut_second_col -> numbers");
 
 my %adj = $m->adjacent($start);
-diag "Adj is %adj\n";
 is_deeply({$m->adjacent($start)},
           {cut_first_col => $letters,
            cut_second_col => $numbers},
@@ -89,9 +77,31 @@ is_deeply({$m->adjacent($letters | $numbers | $combined)},
               sort => $letters | $numbers | $combined | $sorted},
           "States reachable from letters | numbers | combined");
 
-
 my @plan = qw(cut_first_col cut_second_col cat_cols sort);
 
 is_deeply($m->generate(), \@plan, "Generate plan");
+
+__END__
+
+my $m = RUM::StateMachine->new(
+    state_flags => [qw(letters numbers combined sorted)],
+    instructions => [qw(cut_first_col
+                        cut_second_col
+                        cat_cols
+                        sort)],
+    start_flags => [],
+    goal_flags  => [qw(sorted)],
+    transitions => [
+        [[],                     ["letters"],  "cut_first_col"],
+        [[],                     ["numbers"],  "cut_second_col"],
+        [["letters", "numbers"], ["combined"], "cat_cols"],
+        [["combined"],           ["sorted"],   "sort"]]
+);
+
+
+
+
+
+
 
 
