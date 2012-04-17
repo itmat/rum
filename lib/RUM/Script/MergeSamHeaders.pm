f04aeab7fde258a07640f1fcb80eb86cb67efebe
Mike DeLaurentis <delaurentis@gmail.com>
2012-04-17 13:27:33 -0400

    Fix test

--- lib/RUM/Script/MergeSamHeaders.pm
+++ lib/RUM/Script/MergeSamHeaders.pm
@@ -5,13 +5,28 @@ no warnings;
 use Carp;
 use RUM::Sort qw(by_chromosome);
 
+use Getopt::Long;
+use RUM::Usage;
+use RUM::Logging;
+
+our $log = RUM::Logging->get_logger();
+
 sub main {
+
+    GetOptions(
+        "help|h"    => sub { RUM::Usage->help },
+        "quiet|q"   => sub { $log->less_logging(1) },
+        "verbose|v" => sub { $log->more_logging(1) });
+
     local $_;
     my %header;
-    while (defined($_ = <ARGV>)) {
-        chomp;
-        /SN:([^\s]+)\s/ or croak "Bad SAM header $_";
-        $header{$1}=$_;
+    for my $filename (@ARGV) {
+        open my $in, "<", $filename;
+        while (defined($_ = <$in>)) {
+            chomp;
+            /SN:([^\s]+)\s/ or croak "Bad SAM header $_";
+            $header{$1}=$_;
+        }
     }
     for (sort by_chromosome keys %header) {
         print "$header{$_}\n";
