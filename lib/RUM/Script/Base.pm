da14f9251568e1ecac3ad4ac816cc1c4aa7325da
Mike DeLaurentis <delaurentis@gmail.com>
2012-07-13 10:21:53 -0400

    Change base script to get correct logger

--- lib/RUM/Script/Base.pm
+++ lib/RUM/Script/Base.pm
@@ -3,9 +3,16 @@ package RUM::Script::Base;
 use strict;
 use warnings;
 
+use Scalar::Util qw(blessed);
+
 sub logger {
     my ($self) = @_;
-    return $self->{logger} ||= RUM::Logging->get_logger;
+    my $package = blessed($self);
+    return $self->{logger} ||= RUM::Logging->get_logger($package);
+}
+
+sub options {
+    
 }
 
 1;
