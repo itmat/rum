ffb00b83c9e03b3559747e37e6cbad6935e50f1b
Mike DeLaurentis <delaurentis@gmail.com>
2012-07-18 13:24:48 -0400

    Bump version number

--- lib/RUM/Pipeline.pm
+++ lib/RUM/Pipeline.pm
@@ -11,9 +11,9 @@ RUM::Pipeline - RNASeq Unified Mapper Pipeline
 
 =head1 VERSION
 
-Version 2.00_12
+Version 2.0.1_01
 
 =cut
 
-our $VERSION = 'v2.0.1';
-our $RELEASE_DATE = "June 27, 2012";
+our $VERSION = 'v2.0.1_01';
+our $RELEASE_DATE = "July 18, 2012";
