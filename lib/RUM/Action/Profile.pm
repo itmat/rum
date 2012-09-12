96aede3f33b61ad430058ef692b2c7495ab6ae78
Mike DeLaurentis <delaurentis@gmail.com>
2012-09-12 10:16:37 -0400

    Produce HTML file when profiling job

--- lib/RUM/Action/Profile.pm
+++ lib/RUM/Action/Profile.pm
@@ -23,56 +23,168 @@ use Carp;
 use List::Util qw(max sum);
 use Cwd qw(realpath);
 
-
-sub run {
-    my ($class) = @_;
-    my $self = $class->new(name => 'profile');
-
-    GetOptions(
-        "output|o=s" => \(my $dir)
-    );
-
-    my @all_events;
-    
+sub load_events {
+    my ($dir) = @_;
+    my @events;
     find sub {
         return if ! /rum(_\d\d\d)?\.log/;
-        my $events = $self->parse_log_file($File::Find::name);
-        push @all_events, @{ $events };
+        my $events = parse_log_file($File::Find::name);
+        push @events, @{ $events };
     }, "$dir/log";
+    return \@events;
+}
 
-    my $timings = $self->build_timings(\@all_events);
-    my $names = $self->ordered_steps($timings);
-    my $times = $self->times_by_step($timings);
+sub merge_names {
+    my (@name_lists) = @_;
 
-    my @names = @{ $names };
-    my (@max, @sum, @avg);
+    my @result;
+    my %seen;
 
-    for my $name (@names) {
-        my @times = @{ $times->{$name} };
-        push @max, max(@times);
-        push @sum, sum(@times);
-        push @avg, sum(@times) / @times;
-    }
-
-    push @names, 'Whole job';
-    push @max,   sum(@max);
-    push @sum,   sum(@sum);
-    push @avg,   sum(@avg);
-
-    printf "%50s %10s %10s %10s\n", 'Step', 'Max', 'Sum', 'Agv';
-    for my $i (0 .. $#names) {
-        if ($i == $#names) {
-            my $len = 50 + 1 + 10 + 1 + 10 + 1 + 10;
-            print '-' x $len, "\n";
+    for my $names (@name_lists) {
+        for my $name (@{ $names }) {
+            push @result, $name unless $seen{$name}++;
         }
-        printf "%50s %10d %10d %10d\n", $names[$i], $max[$i], $sum[$i], $avg[$i];
     }
+    return @result;
+}
 
+sub rename_steps {
+    my ($times, $step_mapping) = @_;
+
+    my %result;
+    
+
+    for my $old_step (keys %{ $times }) {
+
+        my @times = @{ $times->{$old_step} };
+
+        if (exists $step_mapping->{$old_step} ) {
+            my $new_step = $step_mapping->{$old_step};
+            print "'$old_step' => '$new_step'\n";
+            print "Times was @times\n";
+            if (my $old_times = $result{$new_step}) {
+                @times = map { $old_times->[$_] + $times[$_] } (0 .. $#times );
+            }
+            print "Is now @times\n";
+            $result{$new_step} = \@times;
+        }
+        else {
+            $result{$old_step} = \@times;
+        }
+
+    }
+    return \%result;
 
 }
 
+sub times_for_step {
+    my ($times_for_step, $step) = @_;
+    if (my $times = $times_for_step->{$step}) {
+        return @{ $times };
+    }
+    return;
+}
+
+sub max_time_for_step {
+    my ($times_for_step, $step) = @_;
+    return max times_for_step($times_for_step, $step);
+}
+
+sub total_time_for_step {
+    my ($times_for_step, $step) = @_;
+    return sum times_for_step($times_for_step, $step);
+}
+
+sub avg_time_for_step {
+    my ($times_for_step, $step) = @_;
+    my @times = times_for_step($times_for_step, $step);
+    if (@times) {
+        return sum(@times) / @times;
+    }
+    return;
+}
+
+sub rename_names {
+    my ($name_mapping, @names) = @_;
+    return map { exists $name_mapping->{$_} ? $name_mapping->{$_} : $_ } @names;
+}
+
+sub run {
+    my ($class) = @_;
+
+    my @dirs = @ARGV;
+
+    my $name_mapping = {
+        'Run bowtie on genome'       => 'Run Bowtie on genome',
+        'Parse genome Bowtie output' => 'Run Bowtie on genome',
+        'Run bowtie on transcriptome'       => 'Run Bowtie on transcriptome',
+        'Parse transcriptome Bowtie output' => 'Run Bowtie on transcriptome',
+        'Run blat on unmapped reads'        => 'Run BLAT',
+        'Parse blat output'                 => 'Run BLAT',
+        'Run mdust on unmapped reads'                 => 'Run BLAT',
+
+    };
+
+    my @event_lists  = map { load_events($_)   }  @dirs;
+    my @timing_lists = map { build_timings($_) } @event_lists;
+    my @name_lists   = map { ordered_steps($_) } @timing_lists;
+    my @time_lists   = map { times_by_step($_) } @timing_lists;
+
+    print "Before names are " . Dumper(\@name_lists);
+    @name_lists = map { [ rename_names($name_mapping, @$_) ] } @name_lists;
+    print "After names are " . Dumper(\@name_lists);
+    my @names = merge_names(@name_lists);
+
+    my (@max, @sum, @avg);
+
+
+            
+    @time_lists = map { rename_steps($_, $name_mapping) } @time_lists;
+
+    
+    open my $html, '>', "rum_profile.html";
+
+    print $html "<html><head></head><body><table>";
+
+    print $html "<tr>\n";
+    print $html '<th>Step</th>';
+    
+    for my $times (@time_lists) {
+        print $html '<th>Max</th><th>Sum</th><th>Avg</th>';
+    }
+    print $html '</tr>';
+
+    for my $i (0 .. $#names) {
+        print $html '<tr>';
+
+        my $step = $names[$i];
+        print $html "<td>$step</td>";
+
+        for my $times (@time_lists) {
+            my $print_td = sub {
+                my $val = shift;
+                my $fmt = shift || '%s';
+                if (defined $val) {
+                    printf $html "<td";
+                    printf $html ">$fmt</td>", $val;
+                }
+                else {
+                    printf $html "<td></td>";
+                }
+            };
+
+            $print_td->(max_time_for_step($times, $step));
+            $print_td->(total_time_for_step($times, $step));
+            $print_td->(avg_time_for_step($times, $step), '%.2f');
+        }
+        print $html '</tr>';
+    }
+
+    print $html '</table></body></html>';
+}
+
 sub parse_log_file {
-    my ($self, $filename) = @_;
+    my ($filename) = @_;
     print "Parsing $filename\n";
     open my $in, '<', $filename;
 
@@ -102,7 +214,7 @@ map from module name to total time.
 =cut
 
 sub build_timings {
-    my ($self, $events) = @_;
+    my ($events) = @_;
     
     my @stack;
 
@@ -137,7 +249,7 @@ sub build_timings {
 }
 
 sub ordered_steps {
-    my ($self, $timings) = @_;
+    my ($timings) = @_;
 
     my @steps;
     my %seen;
@@ -152,7 +264,7 @@ sub ordered_steps {
 }
 
 sub times_by_step {
-    my ($self, $timings, $f) = @_;
+    my ($timings, $f) = @_;
 
     my %times;
 
