ec04868d1712e719081526c9e8df2715f93edf4c
Mike DeLaurentis <delaurentis@gmail.com>
2012-09-13 16:50:36 -0400

    Pod

--- lib/RUM/Action.pm
+++ lib/RUM/Action.pm
@@ -67,26 +67,6 @@ sub pipeline {
     return $self->{pipeline};
 }
 
-sub usage_errors { shift->{usage_errors} }
-
-sub check_usage {
-    shift->usage_errors->check;
-}
-
-
-sub get_lock {
-    my ($self) = @_;
-    my $c = $self->config;
-    return if $c->parent || $c->child;
-
-    my $dir = $c->output_dir;
-    my $lock = $c->lock_file;
-    $log->info("Acquiring lock");
-    RUM::Lock->acquire($lock) or die
-          "It seems like rum_runner may already be running in $dir. You can try running \"$0 kill\" to stop it. If you #are sure there's nothing running in $dir, remove $lock and try again.\n";
-}
-
-
 sub show_logo {
     my ($self) = @_;
     my $msg = <<EOF;
@@ -179,45 +159,7 @@ Accepts the following params:
 The name of the action: command the user uses to invoke the action,
 e.g. "align" in "rum_runner align".
 
-=item config (optional)
-
-The RUM::Configuration. If not supplied, will be left undefined, and
-may be loaded based on the B<--output> option when I<get_options> is
-called.
-
 =back
-
-=item $action->get_options(%options)
-
-Parse command line options. %options should be a hash in the format
-expected by Getopt::Long::GetOptions. This method provides a few
-additional options.
-
-=over 4
-
-=item * 
-
-B<-h> and B<--help> are added, and if the user specifies one of them,
-this method prints a usage message and exits.
-
-=item *
-
-B<-o> and B<--output> are added, and the user most provide the output
-directory using one of those options unless they specify B<-h> or
-B<--help>.
-
-=back
-
-=item $action->usage_errors
-
-Return the RUM::Usage object associated with this action. It can be
-used to accumulate usage errors, and then calling
-$action->usage_errors->check_usage will die with an appropriate
-message if usage is bad.
-
-=item $action->check_usage
-
-Equivalent to $action->usage_errors->check_usage;
 
 =back
 
