9b99d027a4ed6f772d108a203da7fc2086b1a5f2
Mike DeLaurentis <delaurentis@gmail.com>
2012-10-26 08:53:54 -0700

    Add to for make_TU_and_TNU

--- lib/RUM/Script/MakeTuAndTnu.pm
+++ lib/RUM/Script/MakeTuAndTnu.pm
@@ -4,59 +4,130 @@ no warnings;
 use autodie;
 
 use RUM::Logging;
-use RUM::Usage;
-use Getopt::Long;
 use RUM::Bowtie;
+use RUM::CommonProperties;
 
 use base 'RUM::Script::Base';
+
 $|=1;
 
 our $log = RUM::Logging->get_logger();
 
-sub main {
+sub summary {
+    return 'Create unique and non-unique mappers from bowtie output'
+}
 
+sub description {
+    return <<'EOF';
+=head2 Input
+
+This script takes the output of a bowtie mapping against the
+transcriptome, which has been sorted by sort_bowtie.pl, and parses it
+to have the four columns:
+
+=over 4
+
+=item 1. read name
+
+=item 2. chromosome
+
+=item 3. span
+
+=item 4. sequence
+
+=back
+
+A line of the (input) bowtie file should look like:
+
+  seq.167a   -   GENE_1321     411    AGATATGATTCACGAAGAGTTAACCCTGATGG
+
+Sequence names are expected to be of the form seq.Na where N in an
+integer greater than 0.  The 'a' signifies this is a 'forward' read,
+and 'b' signifies 'reverse' reads.  The file may consist of all
+forward reads (single-end data), or it may have both forward and
+reverse reads (paired-end data).  Even if single-end the sequence
+names still must end with an 'a'.
+
+
+=head2 Output
+
+The line above is modified by the script to:
+
+  seq.167a   chr14    122572-122588, 122701-122715    AGATATGATTCACGAAG:AGTTAACCCTGATGG
+
+The colon indicates the location of the splice junction.
+
+In the case of single-end reads, if there is a unique such line for
+seq.1a then it is written to the file specified by <tu_filename>.  If
+there are multiple lines for seq.1a then they are all written to the
+file specified by <tnu_filename>.
+
+In the case of paired-end reads the script tries to match up entries for seq.1a and seq.1b consistently, which means:
+
+=over 4
+
+=item 1. both reads are on the same chromosome
+
+=item 2. the two reads map in opposite orientations
+
+=item 3. the start of reads are further apart than ends of reads and
+no further apart than $max_distance_between_paired_reads
+
+=back
+
+If the two reads do not overlap then the consistent mapper is
+represented by two consecutive lines, each with the same sequence name
+except the forward ends with 'a' and the reverse ends with 'b'.  In
+the case that the two reads overlap then they two lines are merged
+into one line and the a/b designation is removed.
+
+If there is a unique set of consistent mappers it is written to the file specified by <tu_filename>.  If there are multiple consistent mappers they are all written to the file specified by <tnu_filename>.  If only the forward or reverse read map then it does not write anything.
+EOF
+}
+
+sub accepted_options {
+    return (
+        RUM::Property->new(
+            opt => 'genes=s',
+            required => 1,
+            desc => 'The file of gene models'),
+        RUM::Property->new(
+            opt => 'index=s',
+            desc => 'The bowtie transcriptome index',
+            required => 1),
+        RUM::Property->new(
+            opt => 'query=s',
+            desc => 'The file of reads',
+            required => 1),
+        RUM::Property->new(
+            opt => 'bowtie-out=s',
+            desc => 'The file output from bowtie'),
+        RUM::Property->new(
+            opt => 'debug'),
+        RUM::CommonProperties->unique_out->set_required,
+        RUM::CommonProperties->non_unique_out->set_required,
+        RUM::CommonProperties->read_type,
+        RUM::CommonProperties->max_pair_dist,
+        RUM::Property->new(
+            opt => 'limit=s',
+            )
+    );
+}
+
+sub run {
+    my ($self) = @_;
+    my $props = $self->properties;
+    my $gene_annot_file = $props->get('genes');
+    my $index = $props->get('index');
+    my $query = $props->get('query');
+    my $query = $props->get('bowtie_out');
+    my $unique_out = $props->get('unique_out');
+    my $non_unique_out = $props->get('non_unique_out');
+    my $debug = $props->get('debug');
     my $self = __PACKAGE__->new;
 
-    $self->get_options(
-        # Input files
-        'genes=s'         => \(my $gene_annot_file),
-        'index=s'         => \(my $index),
-        'query=s'         => \(my $query),
-
-        # Intermediate files
-        'bowtie-out=s'    => \(my $bowtie_out),
-
-        # Output files
-        'unique=s'        => \(my $unique_out),
-        'non-unique=s'    => \(my $non_unique_out),
-
-        # Other params
-        'debug'           => \(my $debug),
-        'single'          => \($self->{single}),
-        'paired'          => \($self->{paired}),
-        'max-pair-dist=s' => \($self->{max_distance_between_paired_reads} = 500000),
-        'limit=s'         => \(my $limit),
-
-
-    );
-
-    $index or RUM::Usage->bad(
-        'Please specify an index with --index');
-
-    $query or RUM::Usage->bad(
-        'Please specify a query with --query');
-
-    $gene_annot_file or RUM::Usage->bad(
-        'Please specify the gene annotation file with --genes');
-
-    $unique_out or RUM::Usage->bad(
-        'Please specify file to write unique mappers with --unique');
-
-    $non_unique_out or RUM::Usage->bad(
-        'Please specify file to write non-unique mappers with --non-unique');
-
-    ($self->{single} xor $self->{paired}) or RUM::Usage->bad(
-        'Please specify exactly one type with either --single or --paired');
+    $self->{single} = $props->get('type') eq 'single';
+    $self->{paired} = $props->get('type') eq 'paired';
 
     my %bowtie_opts = (
         limit => $limit,
