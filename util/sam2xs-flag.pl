f6bf9a610e9c9fd621c36251154e278199589537
Mike DeLaurentis <delaurentis@gmail.com>
2012-11-06 13:35:45 -0500

    Change sam2xs-flag to use RUM::SpliceSignals

--- util/sam2xs-flag.pl
+++ util/sam2xs-flag.pl
@@ -3,7 +3,14 @@
 use strict;
 use warnings;
 use autodie;
+use lib 'lib';
 
+use RUM::SpliceSignals;
+
+my @donor = RUM::SpliceSignals->donor;
+my @donor_rev = RUM::SpliceSignals->donor_rev;
+my @acceptor = RUM::SpliceSignals->acceptor;
+my @acceptor_rev = RUM::SpliceSignals->acceptor_rev;
 
 # Written by Gregory R. Grant
 # University of Pennsylvania, 2010
@@ -18,75 +25,6 @@ Usage: sam2xs-flag.pl <sam file> <genome seq>
 my $genome_sequence = $ARGV[1];
 
 $|=1;
-
-my (@donor, @donor_rev, @acceptor, @acceptor_rev);
-
-# Splice Junctions:
-# ----------------
-# The Canonical:
-#  GTAG
-$donor[0] = "GT";
-$donor_rev[0] = "AC";
-$acceptor[0] = "AG";
-$acceptor_rev[0] = "CT";
-# Other Characterized:
-#  GCAG
-$donor[1] = "GC";
-$donor_rev[1] = "GC";
-$acceptor[1] = "AG";
-$acceptor_rev[1] = "CT";
-#  GCTG
-$donor[2] = "GC";
-$donor_rev[2] = "GC";
-$acceptor[2] = "TG";
-$acceptor_rev[2] = "CA";
-#  GCAA
-$donor[3] = "GC";
-$donor_rev[3] = "GC";
-$acceptor[3] = "AA";
-$acceptor_rev[3] = "TT";
-#  GCCG
-$donor[4] = "GC";
-$donor_rev[4] = "GC";
-$acceptor[4] = "CG";
-$acceptor_rev[4] = "CG";
-#  GTTG
-$donor[5] = "GT";
-$donor_rev[5] = "AC";
-$acceptor[5] = "TG";
-$acceptor_rev[5] = "CA";
-#  GTAA
-$donor[6] = "GT";
-$donor_rev[6] = "AC";
-$acceptor[6] = "AA";
-$acceptor_rev[6] = "TT";
-# U12-dependent:
-#  ATAC
-$donor[7] = "AT";
-$donor_rev[7] = "AT";
-$acceptor[7] = "AC";
-$acceptor_rev[7] = "GT";
-#  ATAA
-$donor[8] = "AT";
-$donor_rev[8] = "AT";
-$acceptor[8] = "AA";
-$acceptor_rev[8] = "TT";
-#  ATAG
-$donor[9] = "AT";
-$donor_rev[9] = "AT";
-$acceptor[9] = "AG";
-$acceptor_rev[9] = "CT";
-#  ATAT
-$donor[10] = "AT";
-$donor_rev[10] = "AT";
-$acceptor[10] = "AT";
-$acceptor_rev[10] = "AT";
-
-#  TAGA
-$donor[11] = "TA";
-$donor_rev[11] = "TA";
-$acceptor[11] = "GA";
-$acceptor_rev[11] = "TC";
 
 sub load_genome {
     my ($genome_filename) = @_;
