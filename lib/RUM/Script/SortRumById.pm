86f1c60fa620550657471fb13820aed981e48e00
Mike DeLaurentis <delaurentis@gmail.com>
2012-12-14 11:48:20 -0500

    Change sort_RUM_by_id to check output file size
    Output file should be the same size as input file. This is an
    easy check to do to make sure none of the temporary files got
    corrupted.

--- lib/RUM/Script/SortRumById.pm
+++ lib/RUM/Script/SortRumById.pm
@@ -4,11 +4,13 @@ use strict;
 no warnings;
 use autodie;
 
+use File::Copy qw(mv);
 use RUM::Usage;
 use RUM::Logging;
 use Getopt::Long;
 
 our $log = RUM::Logging->get_logger();
+
 $|=1;
 
 sub main {
@@ -28,8 +30,8 @@ sub main {
 
     $log->info("Sorting '$infile'");
 
-    $|=1;
-    open INFILE, '<', $infile;
+    my $in_size = `wc -c $infile`;
+
     my $seqnum_prev = 0;
     my $temp1sortedfile   = $infile . "_sorted_temp1";
     my $temp1unsortedfile = $infile . "_unsorted_temp1";
@@ -38,59 +40,65 @@ sub main {
     my $temp3sortedfile   = $infile . "_sorted_temp3";
     my $temp3unsortedfile = $infile . "_unsorted_temp3";
 
-    open OUTFILE1, '>', $temp1sortedfile;
-    open OUTFILE2, '>', $temp1unsortedfile;
-
     my $still_unsorted_flag = 0;
-    while (defined (my $line = <INFILE>)) {
-        $line =~ /^seq.(\d+)/;
-        my $seqnum = $1;
-        if ($seqnum >= $seqnum_prev) {
-            print OUTFILE1 $line;
-            $seqnum_prev = $seqnum;
-        } else {
-            print OUTFILE2 $line;
-            $still_unsorted_flag = 1;
-        }
-    }
-    close(OUTFILE1);
-    close(OUTFILE2);
-    close(INFILE);
 
-    my $num_merges = 0;
-    $still_unsorted_flag = 1;
-    while ($still_unsorted_flag == 1) {
-        $still_unsorted_flag = 0;
+    {
+        open my $in, '<', $infile;
+        open my $outfile1, '>', $temp1sortedfile;
+        open my $outfile2, '>', $temp1unsortedfile;
 
-        $seqnum_prev = 0;
-
-        open INFILE,   '<', $temp1unsortedfile;
-        open OUTFILE1, '>', $temp2sortedfile;
-        open OUTFILE2, '>', $temp2unsortedfile;
-
-        while (defined (my $line = <INFILE>)) {
-            $line =~ /^seq.(\d+)/;
+        while (defined (my $line = <$in>)) {
+            $line =~ /^seq.(\d+)/ or die "Bad line $_";
             my $seqnum = $1;
             if ($seqnum >= $seqnum_prev) {
-                print OUTFILE1 $line;
+                print $outfile1 $line;
                 $seqnum_prev = $seqnum;
             } else {
-                print OUTFILE2 $line;
+                print $outfile2 $line;
                 $still_unsorted_flag = 1;
             }
         }
-        close(OUTFILE1);
-        close(OUTFILE2);
-        close(INFILE);
-        `mv $temp2unsortedfile $temp1unsortedfile`;
+    }
+
+    my $num_merges = 0;
+    $still_unsorted_flag = 1;
+    while ($still_unsorted_flag) {
+        $still_unsorted_flag = 0;
+        $seqnum_prev = 0;
+
+        {
+            open my $in,   '<', $temp1unsortedfile;
+            open my $out1, '>', $temp2sortedfile;
+            open my $out2, '>', $temp2unsortedfile;
+
+            while (defined (my $line = <$in>)) {
+                $line =~ /^seq.(\d+)/;
+                my $seqnum = $1;
+                if ($seqnum >= $seqnum_prev) {
+                    print $out1 $line;
+                    $seqnum_prev = $seqnum;
+                } else {
+                    print $out2 $line;
+                    $still_unsorted_flag = 1;
+                }
+            }
+        }
+
+        mv $temp2unsortedfile, $temp1unsortedfile or die "mv failed: $!";
         merge($temp1sortedfile, $temp2sortedfile, $temp3sortedfile);
         $num_merges++;
     }
 
-    `mv $temp1sortedfile $sortedfile`;
+    mv $temp1sortedfile, $sortedfile or die "mv failed: $!";
     unlink("$temp1unsortedfile");
-    $log->debug("Number of merges required to sort '$infile': $num_merges");
-    $log->debug("Done sorting '$infile' to $sortedfile");
+    $log->info("Number of merges required to sort '$infile': $num_merges");
+
+    my $out_size = `wc -c $sortedfile`;
+
+    if ($out_size != $in_size) {
+        die "Sorted file has different size ($out_size) than input file ($in_size). They should be the same.";
+    }
+    $log->info("Done sorting '$infile' to $sortedfile");
 
 }
 
@@ -99,62 +107,69 @@ sub merge  {
         $temp2sortedfile,
         $temp3sortedfile) = @_;
 
-    open INFILE1, '<', $temp1sortedfile;
-    open INFILE2, '<', $temp2sortedfile;
-    open OUTFILE, '>', $temp3sortedfile;
+    open my $in1, '<', $temp1sortedfile;
+    open my $in2, '<', $temp2sortedfile;
 
     my $flag = 0;
-    my $line1 = <INFILE1>;
+
+    my $line1 = <$in1>;
     chomp($line1);
-    $line1 =~ /^seq.(\d+)/;
+    $line1 =~ /^seq.(\d+)/ or die "Bad line $_";
     my $seqnum1 = $1;
-    my $line2 = <INFILE2>;
+
+    my $line2 = <$in2>;
     chomp($line2);
-    $line2 =~ /^seq.(\d+)/;
+    $line2 =~ /^seq.(\d+)/ or die "Bad line $_";
     my $seqnum2 = $1;
-    if ($line2 eq '') {
+
+    if ( ! $line2 ) {
 	$flag = 1;
 	unlink("$temp2sortedfile");
 	unlink("$temp3sortedfile");
-    } else {
+    }
+    else {
+        open my $out, '>', $temp3sortedfile;
+
 	while ($flag == 0) {
+
 	    while ($seqnum1 <= $seqnum2 && $line1 ne '') {
-		print OUTFILE "$line1\n";
-		$line1 = <INFILE1>;
+		print $out "$line1\n";
+		$line1 = <$in1>;
 		chomp($line1);
 		$line1 =~ /^seq.(\d+)/;
 		$seqnum1 = $1;
-		if ($line1 eq '') {
-		    if ($line2 =~ /\S/) {
+		if ( ! $line1 ) {
+		    if ($line2) {
 			chomp($line2);
-			print OUTFILE "$line2\n";
+			print $out "$line2\n";
 		    }
-		    while ($line2 = <INFILE2>) {
-			print OUTFILE $line2;		    
+		    while ($line2 = <$in2>) {
+			print $out $line2;
 		    }
 		}
 	    }
 	    while ($seqnum2 <= $seqnum1 && $line2 ne '') {
-		print OUTFILE "$line2\n";
-		$line2 = <INFILE2>;
+		print $out "$line2\n";
+		$line2 = <$in2>;
 		chomp($line2);
 		$line2 =~ /^seq.(\d+)/;
 		$seqnum2 = $1;
-		if ($line2 eq '') {
-		    if ($line1 =~ /\S/) {
+		if ( ! $line2 ) {
+		    if ($line1) {
 			chomp($line1);
-			print OUTFILE "$line1\n";
+			print $out "$line1\n";
 		    }
-		    while ($line1 = <INFILE1>) {
-			print OUTFILE $line1;
+		    while ($line1 = <$in1>) {
+			print $out $line1;
 		    }
 		}
 	    }
-	    if ($line1 eq '' && $line2 eq '') {
+	    if ( ! ($line1 || $line2) ) {
 		$flag = 1;
 	    }
 	}
-	`mv $temp3sortedfile $temp1sortedfile`;
+        close $out;
+	mv $temp3sortedfile, $temp1sortedfile or die "mv failed: $!";
 	unlink("$temp2sortedfile");
     }
 }
